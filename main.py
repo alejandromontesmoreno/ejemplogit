65c475be120e9bd9bca3e4a31d4398cfcaa84786
Alejandro Montes Moreno <alejandromontesmoreno@gmail.com>
2026-05-25 09:43:23 -0600

    Versión 4

--- main.py
+++ main.py
@@ -1,3 +1,2 @@
 print('Trabajando con git y GitHub')
 print('Alejandro Montes Moreno')
-print('Uso de la herramienta de git y GitHub para el control de versiones y colaboración en proyectos de programación')
